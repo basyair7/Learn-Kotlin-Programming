ca13fe363e2b360e0d35952d79e018139c6232b4
basyair7 <basyair.fathul@gmail.com>
2024-04-20 22:31:46 +0700

    add projects latihan5

---
+++ latihan5/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package com.basyair7
+
+private val deskripsi: String = """
+Ini merupakan projects latihan ke 5 yaitu Special Class dan Collection
+pada project ini ada terdiri dari Data class, penggunaan data class, 
+copy and modify data class, destructuring declarations, nested class & inner class,
+Enum class, Sealed class, Singleton Object, Companion Object & Anonymous class,
+List, Set dan Map, Collections Operators, Slice, Distinct, dan Chuncked,
+dan Sequences
+""".trimIndent()
+
+private class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(deskripsi)
+        }
+    }
+}

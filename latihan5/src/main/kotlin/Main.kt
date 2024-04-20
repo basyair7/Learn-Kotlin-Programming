99e8f597d9fc0a80aeb8217b1eaef44cc6d58a6f
basyair7 <basyair.fathul@gmail.com>
2024-04-20 22:34:11 +0700

    add projects latihan5

--- latihan5/src/main/kotlin/Main.kt
+++ latihan5/src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ package com.basyair7
 
 private val deskripsi: String = """
 Ini merupakan projects latihan ke 5 yaitu Special Class dan Collection
-pada project ini ada terdiri dari Data class, penggunaan data class, 
+pada project ini ada terdiri dari penggunaan data class, 
 copy and modify data class, destructuring declarations, nested class & inner class,
 Enum class, Sealed class, Singleton Object, Companion Object & Anonymous class,
 List, Set dan Map, Collections Operators, Slice, Distinct, dan Chuncked,
